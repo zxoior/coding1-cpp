fa5e97a1c32870dbbbfd150240a2d280b6590aab
Nathaniel Ayala <nateja.2005@gmail.com>
2026-04-16 11:47:03 -0500

    coding 1 homework start

--- wk3/favs.cpp
+++ wk3/favs.cpp
@@ -9,6 +9,11 @@
 using namespace std;
 
 int main() {
+    // create list
+    vector<string> favs;
+
+    string input;
+
     while(true) {
         cout << "what could you lke to do?\n";
         cout << "You can 'add', 'edit', 'remove', 'show', or 'quit'.\n";
@@ -17,14 +22,26 @@ int main() {
         getline(cin, input);
          
         if(input == "quit") {
-            cout << "Thanks for playing.\n";
+            cout << "Thanks for playing.\n\n";
             break;
         }
         else if(input == "add") {
-            ///
+            cout << "Please add a favorite game; ";
+            // ask the player to type game.
+            getline(cin, input);
+            // get input
+            favs.push_back(input);
+            // add input to list
+        }
+        else if(input == "show") {
+            cout << "\nHere are your favorite games:\n";
+            for(vector<string>::iterator iter = favs.begin(); iter != favs.end(); iter++) {
+                cout << *iter << endl; 
+            }
         }
         else {
             cout << "I dont recognize that input.\n";
         }
     }
+        
 }
