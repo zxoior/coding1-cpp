0608f4240c280a2cff3cb0705518cca3b834b0cf
Nathaniel Ayala <nateja.2005@gmail.com>
2026-04-17 10:34:37 -0500

    more hw work

--- wk3/favs.cpp
+++ wk3/favs.cpp
@@ -9,39 +9,67 @@
 using namespace std;
 
 int main() {
-    // create list
     vector<string> favs;
+    // create list
 
+    vector<string>::iterator chosenFavs;
+    
     string input;
 
+    chosenFavs = find(favs.begin(), favs.end(), input);
+
     while(true) {
-        cout << "what could you lke to do?\n";
-        cout << "You can 'add', 'edit', 'remove', 'show', or 'quit'.\n";
+        cout << "\nWhat could you lke to do?\n";
+        cout << "You can 'add', 'edit', 'remove', 'show', or 'quit'.\n\n";
 
         string input;
         getline(cin, input);
          
         if(input == "quit") {
-            cout << "Thanks for playing.\n\n";
+            cout << "\nThanks for playing.\n\n";
             break;
-        }
+        }   // end of 
         else if(input == "add") {
-            cout << "Please add a favorite game; ";
+            cout << "\nPlease add a favorite game;\n\n";
             // ask the player to type game.
             getline(cin, input);
             // get input
             favs.push_back(input);
             // add input to list
-        }
+        }   // end of add
         else if(input == "show") {
             cout << "\nHere are your favorite games:\n";
             for(vector<string>::iterator iter = favs.begin(); iter != favs.end(); iter++) {
                 cout << *iter << endl; 
             }
-        }
+        }   // end of show
+        else if(input == "remove") {
+            cout << "\nWhat name should we remove from your Favs?\n";
+            string input;
+            cin >> input;
+
+            auto iter = find(favs.begin(), favs.end(), input);
+
+            if(iter != favs.end()) {
+                cout << "Found it!\n";
+                favs.erase(iter);
+            }
+            else {
+                cout << "I could not find that name in favs.\n";
+            }
+        }   // end of remove
+        else if(input == "edit"){
+            cout << "Lets give " << *chosenFavs << " a new name!\n";
+            cin >> input;
+            *chosenFavs = input;
+
+            cout << "Their name is now " << *chosenFavs << ".\n";
+        }   // end of edit
         else {
             cout << "I dont recognize that input.\n";
         }
+
+
     }
         
 }
